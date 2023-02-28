d144df68322a02caefee760487d82a1b2b07e9c7
unknown <vipulsingh.cg@gmail.com>
2023-03-01 05:09:40 +0530

    sol

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -7,4 +7,22 @@ public class Message {
     private String content;
     private Date timestamp;
 
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+        Date date = new Date();
+        this.timestamp = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
 }
